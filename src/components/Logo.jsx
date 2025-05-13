454c27dc43a1b877cda696459069350b357ce51c
chandrashekarR <158203171+chandrashekarRravi@users.noreply.github.com>
2025-05-13 22:02:25 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import logo from '../assets/image.png'
+import Logo from '../assets/image.png'
 
 const Logo = () => {
     const [showMore, setShowMore] = useState(false);
@@ -7,7 +7,7 @@ const Logo = () => {
     return (
         <div className="flex flex-col items-center justify-center pt-8 ">
             <img
-                src={logo}
+                src={Logo}
                 className="w-24 h-24 sm:w-32 sm:h-32 md:w-48 md:h-48 shadow-lg object-contain"
                 alt="Lense and Light Club Logo"
             />
